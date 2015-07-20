6e80a95f9979af07bf9724d466250939e99939b8
Jordan <jayala1993@outlook.com>
2015-07-20 17:24:54 -0400

    Add admin flag to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ User.create!( name:  "Example User",
               password_confirmation: "foobar",
               admin: true)
 
+
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
